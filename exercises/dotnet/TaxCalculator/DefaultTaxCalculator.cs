741643d640098e699d62f212dedffcd70f5714ed
Dalen Chan <Dalen.Chan@brighthr.com>
2022-01-31 13:30:41 +0000

    Added own taxcalculator - passing tests

---
+++ exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxCalculator
+{
+    public class DefaultTaxCalculator : TaxCalculator
+    {
+        public DefaultTaxCalculator(int year) : base(year)
+        {
+
+        }
+
+        public override int CalculateTax(Vehicle vehicle)
+        {
+            if (vehicle.DateOfFirstRegistration.Year == this.Year)
+            {
+                if (vehicle.Co2Emissions >= 1 &&
+                    vehicle.Co2Emissions <= 50)
+                    return 10;
+
+                if (vehicle.Co2Emissions >= 51 &&
+                    vehicle.Co2Emissions <= 75)
+                    return 25;
+
+                if (vehicle.Co2Emissions >= 76 &&
+                    vehicle.Co2Emissions <= 90)
+                    return 105;
+            }
+        }
+    }
+}

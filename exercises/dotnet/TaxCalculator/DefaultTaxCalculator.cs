69282f247b05b21675f3abb09bc30cf9ac4bb971
Dalen Chan <Dalen.Chan@brighthr.com>
2022-01-31 13:43:23 +0000

    All petrol tests passing

--- exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
@@ -26,7 +26,44 @@ namespace TaxCalculator
                 if (vehicle.Co2Emissions >= 76 &&
                     vehicle.Co2Emissions <= 90)
                     return 105;
+
+                if (vehicle.Co2Emissions >= 91 &&
+                    vehicle.Co2Emissions <= 100)
+                    return 125;
+
+                if (vehicle.Co2Emissions >= 101 &&
+                    vehicle.Co2Emissions <= 110)
+                    return 145;
+
+                if (vehicle.Co2Emissions >= 111 &&
+                    vehicle.Co2Emissions <= 130)
+                    return 165;
+
+                if (vehicle.Co2Emissions >= 131 &&
+                    vehicle.Co2Emissions <= 150)
+                    return 205;
+
+                if (vehicle.Co2Emissions >= 151 &&
+                    vehicle.Co2Emissions <= 170)
+                    return 515;
+
+                if (vehicle.Co2Emissions >= 171 &&
+                    vehicle.Co2Emissions <= 190)
+                    return 830;
+
+                if (vehicle.Co2Emissions >= 191 &&
+                    vehicle.Co2Emissions <= 225)
+                    return 1240;
+
+                if (vehicle.Co2Emissions >= 226 &&
+                    vehicle.Co2Emissions <= 255)
+                    return 1760;
+
+                if (vehicle.Co2Emissions > 255)
+                    return 2070;
             }
+
+            return 0;
         }
     }
 }
